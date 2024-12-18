c536d2e25952bd9e1d7db6a5ad6ca6d873daee2b
ninakoceska <108902059+ninakoceska@users.noreply.github.com>
2024-12-18 18:22:24 +0100

    Update api_services.dart

--- Lab2/lab2/lib/services/api_services.dart
+++ Lab2/lab2/lib/services/api_services.dart
@@ -1,34 +1,35 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:mis_lab_2/models/joke.dart';
+import 'package:lab2/models/joke.dart';
 
-class apiServ {
+class ApiService {
   static const String url = "https://official-joke-api.appspot.com";
 
-  Future<List<String>> getTypes() async {
-    var r = await http.get(Uri.parse('$url/types'));
-    if (r.statusCode == 200) {
-      List<dynamic> types = json.decode(r.body);
-      return types.cast<String>();
+  Future<List<String>> JokeType() async {
+    var x = await http.get(Uri.parse('$url/types'));
+    if (x.statusCode == 200) {
+      List<dynamic> list = json.decode(x.body);
+
+      return list.cast<String>();
     }
-    throw Exception('Cannot load types');
+    throw Exception('Cannot load joke types');
   }
 
-  Future<List<Joke>> getJokeOfType(String type) async {
-    var r = await http.get(Uri.parse('$url/jokes/$type/ten'));
-    if (r.statusCode == 200) {
-      List<dynamic> jokes = json.decode(r.body);
-      return jokes.map((joke) => Joke.fromJson(joke)).toList();
+  Future<List<Joke>> JokesByType(String type) async {
+    var x = await http.get(Uri.parse('$url/jokes/$type/ten'));
+    if (x.statusCode == 200) {
+      List<dynamic> list = json.decode(x.body);
+      return list.map((joke) => Joke.fromJson(joke)).toList();
     }
     throw Exception('Cannot load jokes');
   }
 
   Future<Joke> RandomJoke() async {
-    var response = await http.get(Uri.parse('$url/random_joke'));
-    if (response.statusCode == 200) {
-      return Joke.fromJson(json.decode(response.body));
+    var x = await http.get(Uri.parse('$url/random_joke'));
+    if (x.statusCode == 200) {
+      return Joke.fromJson(json.decode(x.body));
     }
-    throw Exception('Failed to load random joke');
+    throw Exception('Cannot load random joke');
   }
 }

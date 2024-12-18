11350665352a4da7d2cbb8494bf892fca561af28
ninakoceska <108902059+ninakoceska@users.noreply.github.com>
2024-12-18 18:38:21 +0100

    Update widget_test.dart

--- Lab2/lab2/test/widget_test.dart
+++ Lab2/lab2/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mis_lab_2/main.dart';
+import 'package:lab2/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
